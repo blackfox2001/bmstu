65833a5d5bcb2c9e15facd39ba3c236c2f644525
blackfox2001 <70910781+blackfox2001@users.noreply.github.com>
2021-12-27 01:21:45 +0300

    Create HomePage.js

---
+++ RIP/labs/lab8/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import HeaderComp from "../components/HeaderComp.js";
+
+function HomePage() {
+    return (
+        <div>
+            <HeaderComp></HeaderComp>
+            <h2>Фильмы</h2>
+            <Link to="/GameTypes"><button>К списку фильмов</button></Link>
+        </div>
+    );
+}
+
+export default HomePage;
